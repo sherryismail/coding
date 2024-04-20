9ed2e88d2d69b07af56f82fb300ef39757652471
sherryismail <sheheera_ismael@live.com>
2024-04-20 15:57:02 +0200

    with conflicts in bitwise operation

--- Cpp_practice/embedded/bitwise.cpp
+++ Cpp_practice/embedded/bitwise.cpp
@@ -18,6 +18,33 @@ bool isPalindrome(unsigned n)
 //https://www.techiedelight.com/power-function-implementation-recursive-iterative/
 //calculate pow(x,n), iteratively recursively and bitwise
  
+bool isPalindrome(unsigned int n)
+{
+    unsigned int rev = 0, original = n;
+    while (n > 0)
+    {
+        rev <<= 1;// bitwise left shift 'rev' by 1 and prepare the LSB
+        if (n&1 == 1)//if LSB is 1
+            rev |= 1;//set bit       
+        n >>= 1;
+    }
+    return (rev == original? true:false);
+}
+//https://www.techiedelight.com/power-function-implementation-recursive-iterative/
+//calculate pow(x,n), iteratively recursively and bitwise
+int pow_recursive(int x, int y)
+{
+    //base case
+    if (x == 0)
+        return 1;
+    else if (x ==1 || y == 1)
+        return x;
+    //recursive case
+    else
+    {
+        return x * pow_recursive(x,y-1);
+    }
+}
 int main()
 {
     unsigned n = 256;
@@ -29,6 +56,11 @@ int main()
 
     int x=5,y=67;
     cout << (x^y) << " is same as XOR operation " <<(x | y) - (x & y)<<endl;
+    n = 9;
+    bool ret = isPalindrome(n);
+    cout <<"isPalindrome("<<n<<") = "<<ret <<endl;
+    x = 2, y=5;
+    cout << "pow_recursive("<<x<<","<<y<<")="<<pow_recursive(x,y)<<endl;
  
     return 0;
 }
